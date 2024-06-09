4b3c16b2f2ba7a62a2b5096ad51b180b1d9babec
ant-agl <aglanton31@gmail.com>
2024-06-10 00:40:30 +0300

    Up

--- js/script.js
+++ js/script.js
@@ -42,3 +42,52 @@ function animateOfferImg() {
 window.addEventListener("scroll", animateOfferImg);
 window.addEventListener("resize", animateOfferImg);
 animateOfferImg();
+
+// form
+const name = document.querySelector('[name="name"]');
+const email = document.querySelector('[name="email"]');
+const phone = document.querySelector('[name="phone"]');
+const message = document.querySelector('[name="message"]');
+const formCallback = document.querySelector(".form-callback");
+formCallback.addEventListener("submit", function (e) {
+  e.preventDefault();
+  let isError = false;
+
+  if (!name.value) {
+    name.classList.add("error");
+    isError = true;
+  }
+  if (!email.value) {
+    email.classList.add("error");
+    isError = true;
+  }
+  if (!phone.value) {
+    phone.classList.add("error");
+    isError = true;
+  }
+  if (!message.value) {
+    message.classList.add("error");
+    isError = true;
+  }
+
+  if (isError) return;
+
+  fetch("https://api.lemon-time.ru/callback", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json;charset=utf-8",
+    },
+    body: {
+      name: name.value,
+      email: email.value,
+      phone: phone.value,
+      text: message.value,
+    },
+  });
+});
+
+document.querySelectorAll("input, textarea").forEach((input) => {
+  input.addEventListener("input", function () {
+    this.classList.remove("error");
+  });
+});
